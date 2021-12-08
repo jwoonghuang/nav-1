ed3a87fd13da5fadd153818ea117ffa20e4b869f
jwoong <jwoong@actionair.cn>
2021-12-08 17:41:38 +0800

    添加随机背景

--- src/main.js
+++ src/main.js
@@ -78,3 +78,33 @@ $(document).on('keypress', (e)=>{
         }
     }
 })
+
+// cover样式生成
+function reset() {
+
+    var hexValues = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e"];
+    
+    function populate(a) {
+      for ( var i = 0; i < 6; i++ ) {
+        var x = Math.round( Math.random() * 14 );
+        var y = hexValues[x];
+        a += y;
+      }
+      return a;
+    }
+    
+    var newColor1 = populate('#');
+    var newColor2 = populate('#');
+    var newColor3 = populate('#');
+    var angle = Math.round( Math.random() * 360 );
+    
+    var gradient = "linear-gradient(" + angle + "deg, " + newColor1 + ", " + newColor2 + ", " + newColor3 + ")";
+    
+    document.getElementById("cover").style.background = gradient;
+    
+  }
+  
+// document.onload = reset();
+$('#set').on('click', ()=>{
+    reset()
+})
